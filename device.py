25f2911607a6f6dd9d9a570d9d6ab6f0c7b7265b
dannywade <danny.wade35@gmail.com>
2021-11-20 11:24:46 -0500

    device demo file added

---
+++ device.py
@@ -0,0 +1,26 @@
+from pyats.topology import Device, Testbed, Interface
+from rich import inspect
+
+# Create and inspect Device object
+device1 = Device("router1")
+inspect(device1, methods=True)
+
+# Create and inspect Interface object
+interface1 = Interface("Eth1/1", "ethernet")  # breakpoint
+inspect(interface1, methods=True)
+
+# Add IPv4 address to Interface object
+interface1.ipv4 = "10.1.1.1"  # breakpoint
+inspect(interface1)
+
+# Add Interface to Device
+device1.add_interface(interface1)  # breakpoint
+inspect(device1)
+
+# Create and inspect Testbed object
+testbed_a = Testbed(name="firstTestbed", alias="myTestbed")  # breakpoint
+inspect(testbed_a, methods=True)
+
+# Add Device to Testbed
+testbed_a.add_device(device1)  # breakpoint
+inspect(testbed_a)
