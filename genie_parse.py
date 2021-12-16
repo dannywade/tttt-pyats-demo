d8b95152f9253c7d5dc11659164799db33a792e9
dannywade <danny.wade35@gmail.com>
2021-12-15 20:32:35 -0500

    added comments

--- genie_parse.py
+++ genie_parse.py
@@ -18,28 +18,33 @@ eve_pass = os.environ["EVE_PASS"]
 
 testbed_file = os.path.join(os.path.dirname(__file__), "testbed.yaml")
 
+# Load the pyATS testbed
 testbed = load(testbed_file)
 
+# Extract just the router-1 device
 r1 = testbed.devices["router-1"]
 
+# Connect to the router-1 device
+# By default, the following commands are usually ran:
+# no logging console, terminal width 511, and others depending on platform
 r1.connect(init_exec_commands=[], init_config_commands=[], log_stdout=False)
-# if r1.connected():  # returns True/False
-#     print("Connected to Router 1!")
-# else:
-#     print("Could not connect to specified device!")
 
+# Collect and parse 'show version' command from router-1 device
+# Using Rich library to pretty print output
 
-# Collect and parse 'show version' command from Router 1 device
+# Print unstructured data - show version output
 show_ver_raw = Pretty(
     r1.execute("show version")
 )  # returns a string value - unstructured
 raw_panel = Panel.fit(show_ver_raw, title="[red]Unstructured Output[/red]")
 print(raw_panel)
 
+# Print structured data - show version output
 show_ver_struct = Pretty(r1.parse("show version"))  # returns a dictionary - structured
 struct_panel = Panel.fit(
     show_ver_struct, title="[turquoise2]Structured Output[/turquoise2]"
 )
 print(struct_panel)
 
+# Disconnect from router-1 device
 r1.disconnect()
